73afbc6daccd6712e54ced853e29ec933fcc81cb
Youssou Diop <youssoudiop@live.com>
2019-06-19 19:21:50 -0400

    Updates

---
+++ precache-manifest.44cdbcc0b40626b959e6349b59de1a88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "3afb8ccc11a3b8d75154",
+    "url": "/clickygame/static/js/main.3afb8ccc.chunk.js"
+  },
+  {
+    "revision": "b17f661fa4d40e9da8c6",
+    "url": "/clickygame/static/js/2.b17f661f.chunk.js"
+  },
+  {
+    "revision": "3afb8ccc11a3b8d75154",
+    "url": "/clickygame/static/css/main.afd4a2a3.chunk.css"
+  },
+  {
+    "revision": "02726448b57278a494393ae6bf60dc40",
+    "url": "/clickygame/index.html"
+  }
+];
